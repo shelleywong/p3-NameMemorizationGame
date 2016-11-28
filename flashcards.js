bffa8bfa74c0d8cf1c08356023c46f727ecd6f59
Shelley Wong <swong26@mail.csuchico.edu>
2016-11-28 09:37:32 -0800

    added a results page with a points cookie

--- flashcards.js
+++ flashcards.js
@@ -11,11 +11,14 @@ var myStudents = [
 var nameInput = []; //empty array for user's text input of student names
 var index = 0; //global index
 var listNum = 1; //one-based index for the user
+var numCorrect = 0;
+var current;
 var studentNum;
 
 //grab student at index & increment index
 function chooseRandom(){
   var image = "";
+  current = index;
   image += listNum + "<img src=\""+myStudents[index].img+"\" width=200>";
   document.getElementById("studentimg").innerHTML = image;
   index++;
@@ -44,6 +47,7 @@ function resetGame(){
   myStudents = shuffle(myStudents);
   index = 0;
   listNum = 1;
+  numCorrect = 0;
   nameInput = [];
   document.getElementById("studentimg").innerHTML = "";
   document.getElementById("result").innerHTML = "";
@@ -51,7 +55,6 @@ function resetGame(){
 }
 
 //enter user-submitted names to an array, then clear the input field for next entry
-//keep selecting a random student until the num of names entered == the num of students
 function submitNames(event){
   if(event.keyCode == 13){
     var input = document.getElementById("nameinput").value;
@@ -59,8 +62,16 @@ function submitNames(event){
     nameInput.push(input);
     console.log(nameInput);
     document.getElementById("nameinput").value = "";
+    //if user answers correctly, increase numCorrect by 1 and adjust the "points" cookie
+    if(nameInput[current] == myStudents[current].firstname.toLowerCase() + " " +
+        myStudents[current].lastname.toLowerCase()){
+          numCorrect += 1;
+    }
+    setCookie("points",numCorrect,1);
+    //keep selecting a random student until the num of names entered is the same
+    //as the num of students. Then go to the results page to check final score
     if(nameInput.length == myStudents.length){
-      checkNames();
+      nextPage('results.html');
     }
     else{
       chooseRandom();
@@ -76,6 +87,12 @@ function pass(){
 
 //after user has submitted all names, check to see if answers are correct matches
 function checkNames(){
+  numCorrect = getCookie("points");
+  var percent = (numCorrect/myStudents.length)*100;
+  percent = percent.toFixed(0);
+  document.getElementById("result").innerHTML = "You got " + numCorrect + " out of "
+    + myStudents.length + " correct <br> " + percent + "%";
+  /*
   var points = 0;
   for(var i=0; i<myStudents.length; i++){
     myStudents[i].firstname = myStudents[i].firstname.toLowerCase();
@@ -85,7 +102,8 @@ function checkNames(){
     }
   }
   document.getElementById("result").innerHTML = "You got " + points + " out of "
-    + myStudents.length + " correct!";
+    + myStudents.length + " correct <br> " + (points/myStudents.length)*100;
+    */
 }
 
 //user chooses name from drop-down list and image of that student appears
@@ -140,4 +158,31 @@ function printAll(){
 
 function nextPage(page){
   window.location.href=page;
+}
+
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname){
+  var name = cname + "=";
+  var c_array = document.cookie.split(';');
+  for(var i = 0; i < c_array.length; i++)
+  {
+    var c = c_array[i];
+    while(c.charAt(0) == ' ')
+    {
+      c = c.substring(1);
+    }
+    if(c.indexOf(name) == 0)
+    {
+      return c.substring(name.length,c.length);
+    }
+  }
+  return "";
 }
